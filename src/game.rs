fde302afe18d94c4be69e36a43d9a308ac297149
Dustin Rohde <dustin.rohde@gmail.com>
2018-07-13 00:38:40 -0400

    ...

--- src/game.rs
+++ src/game.rs
@@ -1,6 +1,7 @@
 use std::default::Default;
 use std::mem;
 use std::thread;
+use std::time::Duration;
 
 use super::grid::{Cell, Grid};
 
@@ -22,7 +23,7 @@ impl Game {
         while !self.grid.is_empty() {
             self.tick();
             println!("{}", self.grid);
-            thread::sleep_ms(1000);
+            thread::sleep(Duration::from_millis(1000));
         }
     }
 
@@ -49,5 +50,35 @@ impl Game {
                 _ => false,
             }
         }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_survives_blinker() {
+        let game = Game::new(Grid::new(vec![Cell(1, 0), Cell(1, 1), Cell(1, 2)]));
+        assert!(
+            game.survives(&Cell(1, 1)),
+            "a live cell with 2 live neighbors should survive"
+        );
+        assert!(
+            game.survives(&Cell(0, 1)),
+            "a dead cell with 3 live neighbors should survive"
+        );
+        assert!(
+            game.survives(&Cell(2, 1)),
+            "a dead cell with 3 live neighbors should survive"
+        );
+        assert!(
+            !game.survives(&Cell(1, 0)),
+            "a live cell with < 2 live neighbors should die"
+        );
+        assert!(
+            !game.survives(&Cell(1, 2)),
+            "a live cell with < 2 live neighbors should die"
+        );
     }
 }
